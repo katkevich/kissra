a6c2caa6f8f2a0543075ce6efe0c8030401abd85
Katkevich Dmitry <rad222on@gmail.com>
2025-12-20 12:00:26 +0200

    fix 'collect_mixin' (use 'forward_like')

--- include/kissra/algo/collect_mixin.hpp
+++ include/kissra/algo/collect_mixin.hpp
@@ -8,8 +8,9 @@ namespace kissra {
 template <typename Tag>
 struct collect_mixin {
     template <template <typename...> typename TTo = std::vector, kissra::mut TSelf>
-    auto collect(this TSelf&& self) {
+    [[nodiscard]] auto collect(this TSelf&& self) {
         using val_t = kissra::iter_value_t<TSelf>;
+        using ref_t = kissra::iter_reference_t<TSelf>;
         using container_t = TTo<val_t>;
 
         container_t result;
@@ -17,11 +18,16 @@ struct collect_mixin {
             result.reserve(self.size());
         }
 
+        /**
+         * `optional<T>` - should move
+         * `optional<T&>` - should copy
+         * `optional<T&&>` - should move
+         */
         while (auto item = self.next()) {
-            if constexpr (kissra::can_push_back<container_t, val_t>) {
-                result.push_back(KISSRA_FWD(*std::move(item)));
-            } else if constexpr (kissra::can_insert<container_t, val_t>) {
-                result.insert(std::ranges::end(result), KISSRA_FWD(*std::move(item)));
+            if constexpr (kissra::can_push_back<container_t, ref_t>) {
+                result.push_back(std::forward_like<ref_t>(*item));
+            } else if constexpr (kissra::can_insert<container_t, ref_t>) {
+                result.insert(std::ranges::end(result), std::forward_like<ref_t>(*item));
             }
         }
 
